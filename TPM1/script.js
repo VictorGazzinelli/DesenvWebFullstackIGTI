7d4d4c9edd41265c99c30fc4d784f09abbb1925f
VictorGazzinelli <vpezzigc@gmail.com>
2020-05-17 14:24:28 -0300

    Update TPM1

--- TPM1/script.js
+++ TPM1/script.js
@@ -1,29 +1,15 @@
+const redRangePicker = document.querySelector("#redRangePicker");
+const blueRangePicker = document.querySelector("#blueRangePicker");
+const greenRangePicker = document.querySelector("#greenRangePicker");
+
 var redValue = 0;
 var greenValue = 0;
 var blueValue = 0;
 
-// window.addEventListener("load", onPageLoad())
+redRangePicker.addEventListener("input", () =>{});
+blueRangePicker.addEventListener("input", () =>{});
+greenRangePicker.addEventListener("input", () =>{});
 
-// function onPageLoad(){
-//     inputRangePickerRed = document.getElementById('redRangePickerValue')
-//     inputRangePickerGreen = document.getElementById('greenRangePicker')
-//     inputRangePickerBlue = document.getElementById('blueRangePicker')
-//     // inputRangePickerRed.addEventListener("keypress", onArrowPress(event , inputRangePickerRed))
-//     // inputRangePickerGreen.addEventListener("keypress", onArrowPress(event, inputRangePickerGreen))
-//     // inputRangePickerBlue.addEventListener("keypress", onArrowPress(event, inputRangePickerBlue))
-// }
-
-// function onArrowPress(event, inputElement){
-//     switch(event.keyCode){
-//         case 37:
-//             inputElement.value = (+(inputElement.value) - 1).toString()
-//             break;
-//         case 39:
-//             inputElement.value = (+(inputElement.value) + 1).toString()
-//         default:
-//             break;
-//     }
-// }
 
 function onRangePickerChange(value, idInput){
     document.getElementById(idInput).value = value;
